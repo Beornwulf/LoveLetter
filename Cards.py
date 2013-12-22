0e4100e667a9259f9cc1f479ea01dc43c9e78fe7
Dominic Sharrock <dominic@sharrock.org.uk>
2013-12-22 00:14:46 +0000

    Create Cards.py

---
+++ Cards.py
@@ -0,0 +1,37 @@
+class Card():
+    """Defines aspects common to all cards."""
+    pass
+
+
+class Princess(Card):
+    """The princess card."""
+    def __init__(self):
+        self.value = 8
+
+class Countess(Card):
+    """The countess card."""
+    pass
+
+class King(Card):
+    """The King card."""
+    pass
+
+class Prince(Card):
+    """The prince card."""
+    pass
+
+class Handmaiden(Card):
+    """The handmaiden card."""
+    pass
+
+class Baron(Card):
+    """The baron card."""
+    pass
+
+class Priest(Card):
+    """The priest card."""
+    pass
+
+class Guard(Card):
+    """The guard card."""
+    pass
